4473cc47a698258a3061bb5578031b2412b6c39e
Tendo Vera <rutsvovera@gmail.com>
2023-08-11 10:56:21 -0700

    adding

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for adding in range(len(sys.argv) - 1):
+        sum += int(sys.argv[adding + 1])
+        print ("{}".format(sum))
