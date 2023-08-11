dabc3e08f9c5ef00bd4f6fc36e8167befdbc7ecf
Tendo Vera <rutsvovera@gmail.com>
2023-08-11 10:47:43 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(count))
+        for number in range(count):
+            print("{}: {}".format(number + 1, sys.argv[number + 1]))
