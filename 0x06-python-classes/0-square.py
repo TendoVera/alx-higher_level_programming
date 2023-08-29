9fcf25593697c3c27535458dd8169b2a23450b92
TendoVera <rutsvovera@gmail.com>
2023-08-29 12:45:13 -0700

    sqaure

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""An empty class Square that defines a square"""
+class Square:
+   """The keyword and name of the class is 'square'
+   which defines the class"""
+   pass
+   """ pass is a line that does not print anything
+   this is done to define a class"""
