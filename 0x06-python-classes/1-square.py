808ce204c50d6539b766a77e23f5ac7e2b9683bd
TendoVera <rutsvovera@gmail.com>
2023-08-29 16:36:12 -0700

    1sqaure

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+class Square:
+  """the class is defined by the name 'sqaure'"""
+def __init__(self,size):
+    self.__size = size
+    """The __init__ method is a constructor and size is used to the size of the square"""
