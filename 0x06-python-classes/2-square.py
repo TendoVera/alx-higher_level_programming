192f12c8fd2c642b0345ecc07c2d7092b90470ab
TendoVera <rutsvovera@gmail.com>
2023-08-30 09:45:04 -0700

    2square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """define a sqaure"""
 class Square:
-      def __init__(self, size=0):
-          """to check size of sqaure.
-          Args:
-            size: the value of size.
-         """
-        if not isinstance(size, int):
-             raise TypeError("size must be an integer")
-         if size < 0:
-             raise ValueError("size must be >= 0")
-         self.__size = size
+  def __init__(self, size=0):
+    """to check size of sqaure.
+    Args:
+    size: the value of size.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
