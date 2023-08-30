8bb8c80fc09f9af3aa7dbd0f1c1a5555fbd75386
TendoVera <rutsvovera@gmail.com>
2023-08-30 10:06:12 -0700

    33sqaure

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """defining a square"""
 class Square:
-    """class named Square"""
-    def __init__(self, size=0):
-        """the area of square is calculated.
-        Args:
-        size: the value of the area.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        def area(self):
-            return (self.__size * self.__size)
+  """class named Square"""
+  def __init__(self, size=0):
+    """the area of square is calculated.
+    Args:
+    size: the value of the area.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+  def area(self):
+    return (self.__size * self.__size)
