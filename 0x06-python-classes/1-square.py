e0f2c35b8c32975d28439d41eaafafca482f79fd
TendoVera <rutsvovera@gmail.com>
2023-08-30 09:40:41 -0700

    1square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,9 @@
 """a class Square that defines a square"""
 class Square:
   """the class is defined by the name 'sqaure'"""
-def __init__(self, size):
+  def __init__(self, size):
     """the inint method is a constructor.
     Args: 
-        size is used for size of square.
+        size (int): length size of square.
         """
     self.__size = size
