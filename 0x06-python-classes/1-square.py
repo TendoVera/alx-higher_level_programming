57bf260cece48c09932a2793e155e256b812bf30
TendoVera <rutsvovera@gmail.com>
2024-02-07 13:44:17 -0800

    1square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module."""
+class Square:
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size):
+        """
+        Args:
+        Parameters: size (int): The size of the square.
+        """
+        self.__size = size
