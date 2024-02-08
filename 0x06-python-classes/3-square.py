e20eeda19b46b2d81431270ce97935642dc221e3
TendoVera <rutsvovera@gmail.com>
2024-02-07 16:32:12 -0800

    3square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+"""Represent a square."""
 class Square:
-    """Represent a square."""
+    """Class defines a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
