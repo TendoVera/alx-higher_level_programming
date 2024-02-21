be208d41e74a3888760d4827ac8a5ef1e844ba66
TendoVera <rutsvovera@gmail.com>
2024-02-21 15:16:58 -0800

    4inherit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to compare to.
+    Returns:
+        True if obj is an inherited instance of a_class, otherwise false.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
