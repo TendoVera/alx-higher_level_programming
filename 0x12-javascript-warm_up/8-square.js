bbfdf5669b6618d762bace9537ddad6c83b9ee2f
Tendo Vera <rutsvovera@gmail.com>
2024-05-02 11:03:18 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const size = parseInt(arg);
+
+if (!isNaN(size)) {
+    if (size > 0) {
+        for (let i = 0; i < size; i++) {
+            console.log("X".repeat(size));
+        }
+    } else {
+        console.log("Missing size");
+    }
+} else {
+    console.log("Missing size");
+}
